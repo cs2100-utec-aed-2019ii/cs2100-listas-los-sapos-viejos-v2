8ef7684aefd54aa47095b6133999c548cba7b290
GSR <39766479+fresh-avocado@users.noreply.github.com>
2019-09-18 16:32:27 -0500

    feat(): cmakebuilddebug and CMake.txt

---
+++ cmake-build-debug/CMakeFiles/Node.h.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Node.h.dir/main.cpp.o"
+  "Node.h.pdb"
+  "Node.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Node.h.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
